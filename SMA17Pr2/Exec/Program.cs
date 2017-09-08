fc358f7cb5686f852a9ce8763a56a5b42827d4ed
swcurran489 <seanwilliamcurran@gmail.com>
2017-09-08 15:17:56 -0400

    derp

---
+++ SMA17Pr2/Exec/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exec {
+	class Program {
+		static void Main(string[] args) {
+		}
+	}
+}
