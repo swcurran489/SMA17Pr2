4bc59162107b13ae60db98bdb51a649a9f3883dc
swcurran489 <seanwilliamcurran@gmail.com>
2017-09-15 16:16:31 -0400

    derp

---
+++ SMA17Pr2/TestServer/TestServer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SMA17Pr2 {
+	class TestServer {
+		static void Main(string[] args) {
+		}
+	}
+}
