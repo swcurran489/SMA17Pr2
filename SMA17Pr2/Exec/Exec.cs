7cb3492547d06d4a84d9ec01ef26413d3a74739e
sean <seanwilliamcurran@gmail.com>
2017-09-26 23:34:09 -0400

    derp

--- SMA17Pr2/Exec/Exec.cs
+++ SMA17Pr2/Exec/Exec.cs
@@ -8,16 +8,16 @@ namespace SMA17Pr2 {
 	public class Exec {
 
 		static void Main(string[] args) {
-			string appBase, repoPath, failProj, builderName, failProjPath, successProj, successProjPath;
+			string appBase, repoPath, failProj, builderPath, failProjPath, successProj, successProjPath;
 			appBase = Path.GetFullPath(@"../../../../");
 			repoPath = Path.Combine(appBase, @"repository");
 			failProj = "test_project";
 			successProj = "herpderp";
 			failProjPath = Path.Combine(appBase, failProj);
 			successProjPath = Path.Combine(appBase, successProj);
-			builderName = @"build_server";
-			Repository r = new Repository(repoPath);
-			Builder b = new Builder(appBase, builderName, repoPath);
+			builderPath = Path.Combine(appBase, @"build_server");
+			Repository r = new Repository(repoPath, builderPath);
+			Builder b = new Builder(builderPath, repoPath);
 			b.init();
 			Console.WriteLine("beginning build fail project\n");
 			r.storeProject(failProjPath);
@@ -26,6 +26,9 @@ namespace SMA17Pr2 {
 			Console.WriteLine("beginning build success project\n");
 			r.storeProject(successProjPath);
 			b.runBuild(successProj);
+			Console.WriteLine("beginning test request");
+			r.createTestRequest("herpderp");
+
 		}
 	}
 }
